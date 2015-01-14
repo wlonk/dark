08423f24df14ec88c8a8c6da36f99e8c120d62ed
Kit La Touche <kit@simpleenergy.com>
2015-01-14 15:09:02 -0700

    Renamed powers, added ability and advantage integrity and display.

--- app/models/face-card.js
+++ app/models/face-card.js
@@ -4,15 +4,30 @@ export default DS.Model.extend({
     name: DS.attr('string'),
     suit: DS.belongsTo('suit'),
     value: DS.attr('number', {defaultValue: 4}),
-    power1: DS.attr('string'),
-    power2: DS.attr('string'),
-    power3: DS.attr('string'),
+    ability: DS.attr('string'),
+    advantage1: DS.attr('string'),
+    advantage2: DS.attr('string'),
+    advantage3: DS.attr('string'),
 
     maxIntegrity: function () {
         if (this.get('max') < this.get('value')) {
             this.set('value', this.get('max'));
         }
     }.observes('suit.baseCards.value'),
+
+    abilityIntegrity: function () {
+        if (this.get('cannotHaveAbility')) {
+            this.set('ability', '');
+        }
+    }.observes('value'),
+
+    advantagesIntegrity: function () {
+        if (!this.get('hasAbility')) {
+            this.set('advantage1', '');
+            this.set('advantage2', '');
+            this.set('advantage3', '');
+        }
+    }.observes('ability'),
 
     max: function () {
         return Math.min(
@@ -21,23 +36,31 @@ export default DS.Model.extend({
         );
     }.property('suit.baseCards.value', 'value'),
 
-    hasPower1: function () {
-        return !!this.get('power1');
-    }.property('power1'),
+    cannotHaveAbility: function () {
+        return this.get('value') < 5;
+    }.property('value'),
 
-    hasPower2: function () {
-        return !!this.get('power2');
-    }.property('power2'),
+    hasAbility: function () {
+        return !!this.get('ability');
+    }.property('ability'),
 
-    hasPower3: function () {
-        return !!this.get('power3');
-    }.property('power3'),
+    hasAdvantage1: function () {
+        return !!this.get('advantage1');
+    }.property('advantage1'),
+
+    hasAdvantage2: function () {
+        return !!this.get('advantage2');
+    }.property('advantage2'),
+
+    hasAdvantage3: function () {
+        return !!this.get('advantage3');
+    }.property('advantage3'),
 
     totalXp: function () {
-        var power1 = this.get('hasPower1') ? 1 : 0;
-        var power2 = this.get('hasPower2') ? 1 : 0;
-        var power3 = this.get('hasPower3') ? 1 : 0;
-        var powerSum = power1 + power2 + power3;
-        return parseInt(this.get('value')) + powerSum - 4;
-    }.property('value', 'power1', 'power2', 'power3')
+        var advantage1 = this.get('hasAdvantage1') ? 1 : 0;
+        var advantage2 = this.get('hasAdvantage2') ? 1 : 0;
+        var advantage3 = this.get('hasAdvantage3') ? 1 : 0;
+        var advantageSum = advantage1 + advantage2 + advantage3;
+        return parseInt(this.get('value')) + advantageSum - 4;
+    }.property('value', 'advantage1', 'advantage2', 'advantage3')
 });
