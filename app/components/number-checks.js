0aaf97370258a8ad0d13fdd7ab8df1dc76b545cb
Kit La Touche <kit.la.t@gmail.com>
2015-01-14 00:25:28 -0700

    Aces with checkbox series.

--- app/components/number-checks.js
+++ app/components/number-checks.js
@@ -2,12 +2,14 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
     model: null,
+    min: null,
+    max: null,
 
     values: function () {
-        var min = 4;
+        var min = this.get('min');
         var clickableMax = this.get('model.max');
         var current = this.get('model.value');
-        var max = 10;
+        var max = this.get('max');
         var arr = Array.apply(null, Array(max - min)).map(function (_, i) {return i + min + 1;});
         return arr.map(function (e) {
             return {
