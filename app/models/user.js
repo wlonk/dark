3d2b2d6ac4e3323fb276e969bcf5e297e6d8ee9e
Kit La Touche <kit.la.t@gmail.com>
2015-01-24 17:52:00 -0700

    Corrected typo on user attribute.

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,6 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
     username: DS.attr('string'),
-    displayceName: DS.attr('string'),
+    displayName: DS.attr('string'),
     sheets: DS.hasMany('sheet')
 });
