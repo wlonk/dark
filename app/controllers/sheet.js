002b860e1f97f7e0bc8fda8b6d8047b406a93260
Kit La Touche <kit.la.t@gmail.com>
2015-02-04 18:30:49 -0700

    Improved save action.
     - More reliable keyboard shortcuts
     - Modal overlay on slow actions

--- app/controllers/sheet.js
+++ app/controllers/sheet.js
@@ -4,12 +4,22 @@ import KeymasterMixin from "../mixins/keymaster";
 export default Ember.ObjectController.extend(
     KeymasterMixin,
     {
+        hideModalOverlay: function () {
+            Ember.$('.modal-backdrop').height(0);
+        },
+
+        showModalOverlay: function () {
+            var doc_height = Ember.$(document).height();
+            Ember.$('.modal-backdrop').height(doc_height);
+        },
+
         shortcuts: {
             'control+s, command+s': 'saveSheet'
         },
 
         actions: {
             saveSheet: function () {
+                this.showModalOverlay();
                 var model = this.get('model');
                 model.get('skillGroups').forEach(function (skillGroup) {
                     skillGroup.get('skills').forEach(function (skill) {
@@ -23,7 +33,7 @@ export default Ember.ObjectController.extend(
                     suit.get('ace.content').save();
                     suit.get('baseCard.content').save();
                 });
-                this.get('model').save();
+                this.get('model').save().then(this.hideModalOverlay);
             }
         }
     }
--- app/mixins/keymaster.js
+++ app/mixins/keymaster.js
@@ -3,7 +3,12 @@ import Ember from "ember";
 export default Ember.Mixin.create({
     init: function () {
         this._super();
+        key.filter = function (event) {
+            // We want shortcuts globally for now; this may eventually change.
+            return true;
+        }
         _.each(this.shortcuts, function (action, shortcut) {
+            console.log("Binding " + action + " to " + shortcut)
             key(shortcut, "all", function (event, handler) {
                 event.preventDefault();
                 this.send(action);
